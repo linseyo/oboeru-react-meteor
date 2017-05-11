c50588865e59cb41568091761235c01092655787
linseyo <reedlinsey@gmail.com>
2017-05-11 11:24:10 -0700

    add Mocha JS test framework

--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -1,10 +1,40 @@
 import React, { Component, PropTypes } from 'react';
+import { Tasks } from '../api/tasks.js';
+
 
 // Task component - represents a single todo item
 export default class Task extends Component {
+  toggleChecked() {
+    // Set the checked property to the opposite of its current value
+    // Update takes in two arguments (selector, update param)
+    Tasks.update(this.props.task._id, {
+      $set: { checked: !this.props.task.checked },
+    });
+  }
+
+  deleteThisTask() {
+    Tasks.remove(this.props.task._id);
+  }
+
   render() {
+    // Assigns a new className to each new task that is checked
+    const taskClassName = this.props.task.checked ? 'checked' : '';
+
     return (
-      <li>{this.props.task.text}</li>
+      <li className={taskClassName}>
+        <button className="delete" onClick={this.deleteThisTask.bind(this)}>
+          &times;
+        </button>
+
+        <input
+          type="checkbox"
+          readOnly
+          checked={this.props.task.checked}
+          onClick={this.toggleChecked.bind(this)}
+        />
+
+        <span className="text">{this.props.task.text}</span>
+      </li>
     );
   }
 }
